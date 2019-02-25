262cd710f159885d86986d1172baf1bdca0e8749
qualname <mhegyi@pm.me>
2019-02-26 00:06:11 +0100

    controlling both characters

--- test.py
+++ test.py
@@ -11,15 +11,16 @@ state_path = Path(f'states/{state_name}.state').resolve()
 
 env = retro.make(env_name,
     use_restricted_actions=retro.Actions.ALL,
-    state=str(state_path))
+    state=str(state_path),
+    players=2)
 
 state = env.reset()
 while True:
     for i in range(100000):
-        _ = env.step(f'{0:012b}')
+        _ = env.step(f'{0:012b}' + f'{0:012b}')
         env.render()
         sleep(1/45)
-        _ = env.step(f'{1024:012b}')
+        _ = env.step(f'{1024:012b}' + f'{8:012b}')
         env.render()
         sleep(1/45)
     env.reset()
